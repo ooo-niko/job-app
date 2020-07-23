73267ec88381d2db6091692a4d9365a516b814f8
大井　優平 <6666nikonana@gmail.com>
2020-07-23 17:30:15 +0900

    ボード作成デザイン完了

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,16 @@
+class BoardsController < ApplicationController
+  def index
+    @boards = Board.all
+  end
+
+  def show
+    @board = Board.find(params[:id])
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+  end
+end
